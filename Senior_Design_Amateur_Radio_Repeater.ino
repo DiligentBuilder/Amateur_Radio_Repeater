578f61f32ec69b6ccd36103b96e2d46c2e1c859b
DiligentBuilder <71938320+DiligentBuilder@users.noreply.github.com>
2024-02-12 17:23:21 -0500

    Adding updated code file, recording functionality
    Started working on the recording functionality, start, stop, and play back recordings. Used the code in the datasheet for the XFS5152CE to help develop the code for the recording functionality we will need and for our applications.

--- Senior_Design_Amateur_Radio_Repeater.ino
+++ Senior_Design_Amateur_Radio_Repeater.ino
@@ -2,10 +2,54 @@
 
 #include <SoftwareSerial.h>
 
+#include "Codec.h"
+
+#include "StdAfx.h"
+
+static FILE * Fp;
+
+static CODEECMODEID g_systate;
+
+static VOLUMEID g_volume;
+
 SoftwareSerial ss(7,8);
 
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+// Start recording coding
+
+void StartEncode (void) {
+  VOLUMEID volume = BR_volume7;
+  g_systate = Codec_encode;
+  g_volume = BR_volume7;
+  Fp = fopen("codec.bts", "wb");
+
+  // start coding, set the sample rate to 16k, the bit rate to 5, and the volume to 7
+  Codec_Start(g_systate, SR_16000, BR_bitrate5, app_sendcommand);
+
+  while(1) {
+    // if the data processing returns non-byte 0, then there is an error, quit
+    if (Codec_Byte_Proc(UART_Get_byte())) {
+      Codec_Stop();
+    }
+
+    // Need to exit
+    if (g_systate != Codec_encode) {
+      Codec_Stop();
+    }
+
+    // Whether to reset the volume
+    if (g_volume != volume) {
+      g_volume = volume;
+      Codec_SetVolume(g_volume);
+
+    }
+  }
+
+  fclose(Fp);
+  
+}
 
 void setup() {
   Serial.begin(115200);
