a83160c0148fd4ac2f144278bc66076402361561
DiligentBuilder <71938320+DiligentBuilder@users.noreply.github.com>
2024-02-16 17:28:20 -0500

    In the debugging process, most recent code

--- Project_Code_2024_02_09_2234.ino
+++ Project_Code_2024_02_09_2234.ino
@@ -5,10 +5,29 @@
 const byte ledPin = 13;
 const byte estPin = 3;
 const byte interruptPin = 5;
+
+// action states for interrupts
 volatile byte state = LOW;
+volatile boolean actionState = LOW;
 
 // variable for adjustable time delay
 const int tdelayadjustable = 30;
+
+
+// declare variables for decoding
+bool Q1_state;
+bool Q2_state;
+bool Q3_state;
+bool Q4_state;
+
+int decimal_value;
+
+String digit_string;
+
+
+
+
+
 
 LiquidCrystal_I2C lcd(0x27,16,2);  // I2C address of LCD chip is 0x27. LCD is 16 chars and 2 lines.
 
@@ -27,35 +46,40 @@ void busy() {
 }
 
 void decodeDTMF() {
+      // change action state of interrupt for decodeDTMF
+      actionState != actionState;
+      
       // DTMF Decoding Code
 
       // wait the adjustable time delay
       delay(tdelayadjustable);
       
 
-      bool ESt_state = digitalRead(3);
+      //bool ESt_state = digitalRead(3);
 
-      bool Q1_state = digitalRead(2);
-      bool Q2_state = digitalRead(4);
-      bool Q3_state = digitalRead(7);
-      bool Q4_state = digitalRead(8);
+      Q1_state = digitalRead(2);
+      Q2_state = digitalRead(4);
+      Q3_state = digitalRead(7);
+      Q4_state = digitalRead(8);
 
       // Calculate the decimal value
-      int decimal_value = 0;
+      decimal_value = 0;
       if (Q4_state) decimal_value += 8; // Q4 represents 8
       if (Q3_state) decimal_value += 4; // Q3 represents 4
       if (Q2_state) decimal_value += 2; // Q2 represents 2
       if (Q1_state) decimal_value += 1; // Q1 represents 1
 
       // Convert the decimal value to a string and send it over serial
-      String digit_string = String(decimal_value);
+      digit_string = String(decimal_value);
 
       // LCD code, print the string of digits onto the LCD display
 
       lcd.clear();
       lcd.setCursor(0,0);
       lcd.print(digit_string);
-      delay(200); // pause for 2 s (2000 ms)
+      lcd.setCursor(0,1);
+      lcd.print("Interrupt detected!");
+      delay(2000); // pause for 2 s (2000 ms)
 }
 
 void setup() {
@@ -72,7 +96,7 @@ void setup() {
   pinMode(interruptPin, INPUT_PULLUP);
 
   // attach interrupt to speech synthesis chip pin
-  attachInterrupt(digitalPinToInterrupt(interruptPin), busy, CHANGE);
+  //attachInterrupt(digitalPinToInterrupt(interruptPin), busy, CHANGE);
 
   // attach interrupt to ESt pin for DTMF decoding
   attachInterrupt(digitalPinToInterrupt(estPin), decodeDTMF, RISING);
@@ -99,15 +123,48 @@ void setup() {
 
 void loop() {
 
+//      // DTMF Decoding Code
+//
+//      // wait the adjustable time delay
+//      // delay(tdelayadjustable);
+//      
+//
+//        bool ESt_state = digitalRead(3);
+//
+//      bool Q1_state = digitalRead(2);
+//      bool Q2_state = digitalRead(4);
+//      bool Q3_state = digitalRead(7);
+//      bool Q4_state = digitalRead(8);
+//
+//      // Calculate the decimal value
+//      int decimal_value = 0;
+//      if (Q4_state) decimal_value += 8; // Q4 represents 8
+//      if (Q3_state) decimal_value += 4; // Q3 represents 4
+//      if (Q2_state) decimal_value += 2; // Q2 represents 2
+//      if (Q1_state) decimal_value += 1; // Q1 represents 1
+//
+//      // Convert the decimal value to a string and send it over serial
+//      String digit_string = String(decimal_value);
+//
+//      // LCD code, print the string of digits onto the LCD display
+//
+//      lcd.clear();
+//      lcd.setCursor(0,0);
+//      lcd.print(digit_string);
+      lcd.setCursor(0, 1);
+      //lcd.print(ESt_state);
+      
+      delay(100); // pause for 0.2 s (200 ms)
+
       
 
       // Speech Synthesis Chip code
 
-      digitalWrite(9, HIGH);
-      speak("[x0][t6][v8][s6][m51][g2][h2][n1]K 3 A U K Testing.");
-      delay(3000);
-      digitalWrite(9, LOW);
-      delay(5000);
+      // digitalWrite(9, HIGH);
+      // speak("[x0][t6][v8][s6][m51][g2][h2][n1]K 3 A U K Testing.");
+      // delay(3000);
+      // digitalWrite(9, LOW);
+      // delay(5000);
 
       
 }
