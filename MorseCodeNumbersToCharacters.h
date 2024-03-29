5ff0322234aeddd4c0255e24d2a637ec68b5b28d
DiligentBuilder <71938320+DiligentBuilder@users.noreply.github.com>
2024-03-29 17:03:38 -0400

    Adding header file to implement the conversion from the string of numbers to the string of characters (letters and numbers corresponding to the morse code represented with the number string)

---
+++ MorseCodeNumbersToCharacters.h
@@ -0,0 +1,8 @@
+// function that converts the Morse Code Numbers into Characters
+String MorseCodeNumbersToCharacters(String new_callsign_string) {
+  String substring;
+
+  for (i = 0; i < new_callsign_string.length(); i++) {
+
+  }
+}
